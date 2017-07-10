0f5f122be1fcd30bcc2d5a41b2fac103721548da
diego <diego@192.168.0.24>
2017-07-09 21:08:21 -0300

    First Commit

--- config/app.php
+++ config/app.php
@@ -142,6 +142,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+// Zofe\Rapyd\RapydServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -200,5 +202,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 ];
